72a821cb5e6aef2d38bb58b0a09bb05bdbf1fa80
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-17 16:37:48 +0700

    demo with List and Map

--- src/DemoCollectionFramework.java
+++ src/DemoCollectionFramework.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class DemoCollectionFramework {
+import java.util.*;
+
+public class DemoCollectionFramework {
+
+    public static void main(String[] args) {
+
+        // Map
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+        employeeMap.put(1, new Employee("f1","l1"));
+        employeeMap.put(2, new Employee("f2","l2"));
+        employeeMap.put(15, new Employee("f15","l15"));
+
+        // Iterate
+        Set<Integer> keys = employeeMap.keySet();
+        for (Integer key: keys) {
+            Employee e = employeeMap.get(key);
+            System.out.println("Data =>" + e.getFullName());
+        }
+
+        //List
+        List<String> users = new ArrayList();
+        users.add("Puwadech");
+        users.add("123");
+
+        for (String user: users) {
+            System.out.println(user);
+        }
+
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(users.get(i));
+        }
+    }
+
 }
