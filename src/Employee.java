f5729b3d24e0e9156b40caa785e279fcdfb0f107
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-17 11:17:10 +0700

    Working with Employee

---
+++ src/Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    //Properties or states
+    private int id;
+    private String fname;
+    private String lname;
+//    Departmentd dep;
+    private String pos;
+
+    //Behaviors or methods
+
+    void doSmt() {
+
+    }
+
+    String getFullName() {
+        return fname + " : "+lname ;
+    }
+
+    String getPositionName() {
+        return "";
+    }
+}
