e17a7942058932b5d497aa9432dcc069e3dcc614
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-17 16:56:00 +0700

    test case 1

---
+++ src/MySort.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class MySort {
+
+    private List<Integer> input ;
+
+    public MySort(List<Integer> input) {
+        this.input = input;
+    }
+
+    public List<Integer> sortCase01(){
+        return input ;
+    }
+}
