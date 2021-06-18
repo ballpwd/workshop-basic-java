69f669108d440eaa81536b4cd92d7c23c51d9ba6
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-18 10:20:34 +0700

    add maven

---
+++ demo01/src/tech/kbtg/a/Demo.java
@@ -0,0 +1,15 @@
+package tech.kbtg.a;
+
+public class Demo {
+
+    public static int count = 20;
+
+    // Constant
+    public static final int XYZ = 1234;
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
