6f1343ab6070727b486216267ed44e17f455e96c
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-21 16:30:47 +0700

    working with json

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,10 +2,7 @@ package com.example.hellorest.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -45,6 +42,11 @@ public class EmployeeController {
         }
 
         return new EmployeeResponse(_id,"Puwadech","Ball") ;
+    }
+
+    @PostMapping("/employee")
+    public EmployeeResponse createNewEmployee(@RequestBody EmployeeRequest request){
+        return new EmployeeResponse(999, request.getFname(),request.getLname());
     }
 
 }
---
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeRequest.java
@@ -0,0 +1,30 @@
+package com.example.hellorest.employee;
+
+public class EmployeeRequest {
+    private String fname;
+    private String lname;
+
+    public EmployeeRequest() {
+    }
+
+    public EmployeeRequest(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public void setFname(String fname){
+        this.fname = fname;
+    }
+
+    public String getFname(){
+        return fname;
+    }
+
+    public void setLname(String lname){
+        this.lname = lname;
+    }
+
+    public String getLname(){
+        return lname;
+    }
+}
