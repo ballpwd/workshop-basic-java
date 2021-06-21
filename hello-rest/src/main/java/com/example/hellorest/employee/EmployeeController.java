5e83481e0fb2b85fa5f6bdab4337bd2bfafce28f
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-21 12:02:10 +0700

    parse int

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -8,8 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     @GetMapping("/employee/{id}")
-    public EmployeeResponse getEmployeeById(@PathVariable  int id) {
-        return new EmployeeResponse(id,"Puwadech","Ball") ;
+    public EmployeeResponse getEmployeeById(@PathVariable  String id) {
+
+        // Validate id => Number only
+        int _id = 0;
+
+        try {
+            _id = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            _id = 0 ;
+        }
+
+
+
+
+        return new EmployeeResponse(_id,"Puwadech","Ball") ;
     }
 
+
 }
+
+
