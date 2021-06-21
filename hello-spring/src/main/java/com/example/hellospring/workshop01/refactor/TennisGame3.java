1a0072a15ed48c25e285abe0e1dcbc6edab0aa7e
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-21 09:52:12 +0700

    Refactor Tennis

--- hello-spring/src/main/java/com/example/hellospring/workshop01/refactor/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/refactor/TennisGame3.java
@@ -1,36 +1,57 @@
 package com.example.hellospring.workshop01.refactor;
 
 public class TennisGame3 {
+    private int player2Score;
+    private int player1Score;
+    private String player1Name;
+    private String player2Name;
+    private String[] point = {"Love", "Fifteen", "Thirty", "Forty"};
+    private String scoredPlayer;
 
-    private int p2;
-    private int p1;
-    private final String p1N;
-    private final String p2N;
-
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (this.isDraw()) {
+            return this.getDraw();
         } else {
-            if (p1 == p2)
+            if (this.isDeuce()) {
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+            }else {
+                return this.getWinner();
+            }
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1"))
-            this.p1 += 1;
+            this.player1Score += 1;
         else
-            this.p2 += 1;
-
+            this.player2Score += 1;
     }
+
+    public boolean isDeuce(){
+        return player1Score == player2Score;
+    }
+
+    public String getPlayer(){
+        return player1Score > player2Score ? player1Name : player2Name;
+    }
+
+    public String getWinner(){
+        this.scoredPlayer = this.getPlayer();
+        return ((player1Score - player2Score)*(player1Score - player2Score) == 1) ? "Advantage " + scoredPlayer : "Win for " + scoredPlayer;
+    }
+
+    public String getDraw(){
+        this.scoredPlayer = this.point[player1Score];
+        return (player1Score == player2Score) ? scoredPlayer + "-All" : scoredPlayer + "-" + point[player2Score];
+    }
+
+    public boolean isDraw(){
+        return player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6);
+    }
+
 }
