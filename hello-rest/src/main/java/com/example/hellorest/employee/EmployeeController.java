45abe960cacd0d31a1d02c2885bcf6432d488667
Puwadech Sillapathadapong <puwadech.s@kbtg.tech>
2021-06-21 14:12:13 +0700

    test

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,15 +17,26 @@ public class EmployeeController {
         try {
             _id = Integer.parseInt(id);
         } catch (NumberFormatException e) {
-            _id = 0 ;
+            // ERROR => TODO ?
         }
-
-
-
 
         return new EmployeeResponse(_id,"Puwadech","Ball") ;
     }
 
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeById2(@RequestParam(defaultValue = "100") String id) {
+
+        // Validate id => Number only
+        int _id = 0;
+
+        try {
+            _id = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            // ERROR => TODO ?
+        }
+
+        return new EmployeeResponse(_id,"Puwadech","Ball") ;
+    }
 
 }
 
---
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+public class EmployeeControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void callApiWithPathVariable(){
+        EmployeeResponse response = restTemplate.getForObject("/employee/123",EmployeeResponse.class);
+        assertEquals(123,response.getId());
+        assertEquals("Puwadech",response.getFname());
+        assertEquals("Ball",response.getLname());
+    }
+
+    @Test
+    public void callApiWithRequestParameter(){
+        EmployeeResponse response = restTemplate.getForObject("/employee?id=999",EmployeeResponse.class);
+        assertEquals(999,response.getId());
+        assertEquals("Puwadech",response.getFname());
+        assertEquals("Ball",response.getLname());
+    }
+
+}
